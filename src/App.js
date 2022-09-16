dfa6b5a5a0a9e48297e8581a7cc2c48694492900
Mahmoud jamal <mahmoud1234goraba@gmail.com>
2022-09-16 21:43:36 +0200

    chatApp with react

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '700b4250-d908-41b1-84c8-1f95e3cb090a';
+const username = 'mahmoud'
+const password = '123456'
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+
+
+export default App;
